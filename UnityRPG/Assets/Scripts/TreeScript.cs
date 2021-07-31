d6cec0d43cfbcb446849c9902248899c250df6c2
OMertens <49765176+GSGZilch@users.noreply.github.com>
2021-07-31 15:31:41 +0200

    Changed name Tree.cs to TreeScript.cs

--- UnityRPG/Assets/Scripts/TreeScript.cs
+++ UnityRPG/Assets/Scripts/TreeScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : MonoBehaviour
+public class TreeScript : MonoBehaviour
 {
     public bool isChopped = false;
     public GameObject log;
